283a09e15081c4e3c72b50becc172734842603e9
novaksam <snovak@uwsp.edu>
2013-02-01 11:51:01 -0600

    Initial completion of packet class

--- CIS499_IM_Server/Packet.cs
+++ CIS499_IM_Server/Packet.cs
@@ -17,34 +17,80 @@ namespace CIS499_IM_Server
         #region Fields
 
         /// <summary>
+        ///     UserID of the target
+        /// </summary>
+        internal uint RecipientId { get; private set; }
+
+        /// <summary>
+        ///     UserID of the sender
+        /// </summary>
+        internal uint SenderId { get; private set; }
+
+        /// <summary>
         ///     Target IP address
         /// </summary>
-        private string IPDst;
+        internal string IpDst { get; private set; }
 
         /// <summary>
         ///     Sender IP address
         /// </summary>
-        private string IPSrc;
+        internal string IpSrc { get; private set; }
 
         /// <summary>
         ///     String containing the message being sent.
         ///     this might be encrypted later, but for now clear text.
         /// </summary>
-        private string Message;
-
-        /// <summary>
-        ///     UserID of the target
-        /// </summary>
-        private uint RecepientID;
-
-        /// <summary>
-        ///     UserID of the sender
-        /// </summary>
-        private uint SenderID;
+        internal string Message { get; private set; }
 
         #endregion
 
-        #region 
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Packet"/> class.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="recep">
+        /// The Recipient.
+        /// </param>
+        /// <param name="mess">
+        /// The mess.
+        /// </param>
+        public Packet(uint sender, uint recep, string mess)
+        {
+            this.SenderId = sender;
+            this.RecipientId = recep;
+            this.Message = mess;
+        }
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Packet"/> class.
+        /// </summary>
+        /// <param name="ipDst">
+        /// The destination IP
+        /// </param>
+        /// <param name="ipSrc">
+        /// The source IP
+        /// </param>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        /// <param name="recipientId">
+        /// The Recipient.
+        /// </param>
+        /// <param name="senderId">
+        /// The sender.
+        /// </param>
+        public Packet(string ipDst, string ipSrc, string message, uint recipientId, uint senderId)
+        {
+            this.IpDst = ipDst;
+            this.IpSrc = ipSrc;
+            this.Message = message;
+            this.RecipientId = recipientId;
+            this.SenderId = senderId;
+        }
 
         #endregion
     }
